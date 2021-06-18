d4ebc2d414eb42b9f69f5dfd126ec60b15b4938d
Forthey <0forthey0@gmail.com>
2021-06-18 15:38:23 +0300

    Теперь move() получает двумерных массив и возвращает координату "съеденного" объекта

--- app/src/main/java/Animal.kt
+++ app/src/main/java/Animal.kt
@@ -1,17 +1,15 @@
-import java.lang.Math.random
-
-class Point(val posX: Int, val posY: Int) {
-}
+class Point(val posX: Int, val posY: Int)
 
 /* Class Animal
  * position - координаты животного (количество клеток от левого верхнего угла)
  * fieldOfView - область видмости (количество клеток от животного во все стороны (в т.ч. по диагонали)
  * speed - скорость животного (количество клеток, которое может пройти животное за один ход)
  */
-abstract class Animal(var position: Point, val fieldOfView: Int, val speed: Int) {
+abstract class Animal(protected var position: Point, val fieldOfView: Int, val speed: Int, var amountFoodForBreeding: Float) {
+    var currentAmountOfFood = 0
     // Функция, задающая координаты животного
     protected fun setPosition(X: Int, Y: Int) {
         position = Point(X, Y)
     }
-    abstract fun move(animals: MutableList<Animal>): Boolean // Функция, реализующая поведение для животного
+    abstract fun move(field: ArrayList<ArrayList<Int>>): Point // Функция, реализующая поведение для животного
 }
