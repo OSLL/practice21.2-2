b8e29a9db2893943b2bd82733b656a835ede5f70
Forthey <0forthey0@gmail.com>
2021-06-18 12:18:38 +0300

    var --> val в Animal.kt

--- app/src/main/java/Animal.kt
+++ app/src/main/java/Animal.kt
@@ -3,7 +3,7 @@ import java.lang.Math.random
 class Point(val posX: Int, val posY: Int) {
 }
 
-abstract class Animal(var position: Point, fieldOfView: Int, speed: Int) {
+abstract class Animal(var position: Point, val fieldOfView: Int, val speed: Int) {
     fun setPosition(X: Int, Y: Int) {
         position = Point(X, Y)
     }
