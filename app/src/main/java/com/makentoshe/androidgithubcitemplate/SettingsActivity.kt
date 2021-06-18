e0881a73939a9c0f1052ddadce05d394b340a560
Dima Tsuranov <dimcur2004@gmail.com>
2021-06-18 15:16:49 +0300

    Попытки вернуть значение из функции-дочки в функцию-мать. Теперь надеюсь, что эта ветка тупиковая и что получится удалить экран настроек на empty activity и заменить его на SettingsActivity

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
@@ -3,17 +3,31 @@ package com.makentoshe.androidgithubcitemplate
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import kotlin.system.exitProcess
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val BackBtn = findViewById<Button>(R.id.BackBtn1)
+        val backBtn = findViewById<Button>(R.id.BackBtn1)
 
-        BackBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        backBtn.setOnClickListener {
+            onDestroy()
+        }
+
+        val predTxt = findViewById<TextView>(R.id.editText2)
+        val saveBtn = findViewById<ImageButton>(R.id.imageButton)
+
+        //  При нажатии на сохранялку (saveBtn): в SavedInstanceState остаётся строка с новыми числовыми параметрами
+        //   (например: количество хищников и травоядных, коэффициент связи увеличения СКОРОСТИ и увеличения РАСХОДА ЭНЕРГИИ). Параметры разделены пробелами
+        saveBtn.setOnClickListener{
+            override fun onSaveInstanceState(outState: Bundle) {
+                outState.putString("NewPredNum", predTxt.toString())
+            }
         }
 
     }
