e3406120040363d0a71c1a00637810615534b8df
Dima Tsuranov <dimcur2004@gmail.com>
2021-06-18 20:58:30 +0300

    !!!Настройки признаны приятными!!!
    (+)Контроль начального количества predators & herbivores и отношения скорости к эффективности
    
    (+)Сохранение этих параметров в памяти до следующего изменения[SharedPreferences()].
    
    (+)Пример получения этих параметров в любой точке программы
    
    Жалко, что всё вместе получилось...
    
    P.S.: есть 18 варнингов, но они выглядят в основном безобидно(есть скрин)

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
@@ -1,13 +1,11 @@
 package com.makentoshe.androidgithubcitemplate
 
-import android.content.Intent
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
-import kotlin.system.exitProcess
+import android.widget.EditText
+import android.widget.Toast
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,19 +14,57 @@ class SettingsActivity : AppCompatActivity() {
         val backBtn = findViewById<Button>(R.id.BackBtn1)
 
         backBtn.setOnClickListener {
-            onDestroy()
+            finish()
         }
 
-        val predTxt = findViewById<TextView>(R.id.editText2)
-        val saveBtn = findViewById<ImageButton>(R.id.imageButton)
+        val sbmtBtnPredNum = findViewById<Button>(R.id.PNBtn)
+        val predTxt = findViewById<EditText>(R.id.editText)
 
-        //  При нажатии на сохранялку (saveBtn): в SavedInstanceState остаётся строка с новыми числовыми параметрами
-        //   (например: количество хищников и травоядных, коэффициент связи увеличения СКОРОСТИ и увеличения РАСХОДА ЭНЕРГИИ). Параметры разделены пробелами
-        saveBtn.setOnClickListener{
-            override fun onSaveInstanceState(outState: Bundle) {
-                outState.putString("NewPredNum", predTxt.toString())
-            }
+        val sbmtBtnHerbNum = findViewById<Button>(R.id.HNBtn)
+        val herbTxt = findViewById<EditText>(R.id.editText2)
+
+        val sbmtBtnVelEff = findViewById<Button>(R.id.VEBtn)
+        val velEffTxt = findViewById<EditText>(R.id.editText3)
+
+        //  При нажатии на сохранялку "Submit ..."(SbmtBtn...): в SharedPreferences остаются строки с целыми числовыми параметрами со своими Ключами(тэгами)
+        //   (например: количество хищников и травоядных, коэффициент связи увеличения СКОРОСТИ и увеличения РАСХОДА ЭНЕРГИИ)
+        val shPrSave = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+        val editor = shPrSave.edit()
+
+        //Количество хищников
+
+        sbmtBtnPredNum.setOnClickListener{
+                editor.apply{
+                    putString("PredNum", predTxt.text.toString())
+                }.apply()
         }
+
+        //Количество травоядных
+        sbmtBtnHerbNum.setOnClickListener{
+            editor.apply{
+                putString("HerbNum", herbTxt.text.toString())
+            }.apply()
+        }
+
+        //Отношение изменения скорости к соответствующему изменению эффективности
+        sbmtBtnVelEff.setOnClickListener{
+            editor.apply{
+                putString("VelEff", velEffTxt.text.toString())
+            }.apply()
+        }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //Пример доступа к полученным значениям (каждое в формате строки)
+        //Примечания: Даже после закрытия приложения данные, сохранённые нажатием Submit, остаются в памяти до следующего изменения.
+        //            В ЛЮБОМ МЕСТЕ в программе можно реализовать следующий код и получить значения!
+
+        val shPrload = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+        Toast.makeText(this,
+            "Predators:" + shPrload.getString("PredNum", null) +
+                "\nHerbivores:" + shPrload.getString("HerbNum", null) +
+                "\nVelocity/Efficiency:" + shPrload.getString("VelEff", null),
+                Toast.LENGTH_SHORT).show() //null - параметр по умолчанию
 
     }
 }
