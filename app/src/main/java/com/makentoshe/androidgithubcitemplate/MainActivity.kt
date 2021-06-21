71e558a13ad75c9da938bfb6ae8b9815b0a3ec5f
Dima Tsuranov <dimcur2004@gmail.com>
2021-06-22 00:27:29 +0300

    Попытка переделать настройки в виде RecyclerView, не получилось сохранить значения из sharedPreferences, но прокрутка заработала

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -3,6 +3,7 @@ package com.makentoshe.androidgithubcitemplate
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("Checkj", "onCreateMain")
         val SettingsBtn = findViewById<Button>(R.id.SettingsBtn)
         val StatsBtn = findViewById<Button>(R.id.StatsBtn)
 
         SettingsBtn.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
+            Log.d("Checkj", "beforeIntent")
             startActivity(intent)
+            Log.d("Checkj", "afterIntent")
         }
         StatsBtn.setOnClickListener {
             val intent = Intent(this, StatsActivity::class.java)
--- app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
@@ -1,18 +1,31 @@
 package com.makentoshe.androidgithubcitemplate
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
+
+class aPreference(var tvCONTENT: String, var tvINDEX: String, var etCONTENT: Float, var etINDEX: String, var btnINDEX: String)
+var settings = mutableListOf<aPreference>()
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("Checkj", "onCreate")
         setContentView(R.layout.activity_settings)
+        Log.d("Checkj", "setContentView")
 
-        val shPrload = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+        /*val shPrload = getSharedPreferences("Settings", Context.MODE_PRIVATE)
 
         val backBtn = findViewById<Button>(R.id.BackBtn1)
 
@@ -86,6 +99,51 @@ class SettingsActivity : AppCompatActivity() {
             "\nPoints for plant:" + shPrload.getFloat("PoForPl", 1f).toString() +
             "\nDefault check:" + shPrload.getString("DeCh", "default text"),
             Toast.LENGTH_SHORT).show() //null - параметр по умолчанию
-
+*/
+        //Через RecyclerView
+        val recyclerView = findViewById<RecyclerView>(R.id.rv)
+        recyclerView.adapter = MyRecyclerViewAdapter()
+        val shPr = getSharedPreferences("Settings", MODE_PRIVATE)
+        /*fun fillSettings(){
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            settings.add(aPreference("Predators number:", "predNumTxt", 5f, "predNum", "predBtn"))
+            settings.add(aPreference("Herbivores number:", "herbNumTxt", 5f, "herbNum", "herbBtn"))
+            settings.add(aPreference("Velocity/Efficiency:", "VelEffTxt", 1f, "VelEff", "VelEffBtn"))
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        }
+        for(i in 0..settings.lastIndex) {
+            val shPr = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+            settings[i].etCONTENT = shPr.getFloat(settings[i].etINDEX, settings[i].etCONTENT)
+        }*/
     }
+}
+
+class MyRecyclerViewAdapter: RecyclerView.Adapter<MyViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder{
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, pos: Int) {
+        holder.info.text = settings[pos].tvCONTENT
+        holder.gettR.setText(settings[pos].etCONTENT.toString())
+        holder.sbmt.setOnClickListener{
+            val shPr:SharedPreferences = this.getSharedPreferences("Settings", Context.MODE_PRIVATE)
+            val editor = shPr.edit()
+            editor.apply{
+                putFloat(settings[pos].etINDEX, settings[pos].etCONTENT)
+            }
+
+        }
+    }
+
+    override fun getItemCount(): Int{
+        return 3
+    }
+}
+
+class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
+    val info: TextView = view.findViewById(R.id.tevi)
+    val gettR: EditText = view.findViewById(R.id.edte)
+    val sbmt: Button = view.findViewById(R.id.sbmt)
 }
